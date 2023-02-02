b047cc08ffff9f61b1745cafc22e82d45cc84be2
IrynaKR-1 <kaletn.ira@gmail.com>
2023-02-02 23:27:25 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+let ingredientName = document.querySelector("#ingredients");
+const createLi = (ingredientsList) => {
+  return ingredientsList.map((ingredient) => {
+    const ingredientsItem = document.createElement("li");
+    ingredientsItem.classList.add("item");
+    ingredientsItem.textContent = ingredient;
+    return ingredientsItem;
+  });
+};
+const elements = createLi(ingredients);
+ingredientName.append(...elements);
