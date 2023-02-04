c0a9a9123aff5a419018201ebed82f4414ad1215
IrynaKR-1 <kaletn.ira@gmail.com>
2023-02-04 11:44:53 +0200

    task - 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    if (value === "" || name === "") {
+      alert("Всі поля повинні бути заповнені!");
+    } else {
+      console.log("onFormSubmit -> name", name);
+      console.log("onFormSubmit -> value", value);
+      form.reset();
+    }
+  });
+}
