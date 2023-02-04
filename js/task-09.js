749dfe943f1625668b9a54f85f3482ef0465b461
IrynaKR-1 <kaletn.ira@gmail.com>
2023-02-04 13:16:01 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnChangeColor = document.querySelector(".change-color");
+const backgroundColor = document.querySelector(".color");
+const body = document.querySelector("body");
+
+function randomChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  backgroundColor.textContent = body.style.backgroundColor;
+}
+btnChangeColor.addEventListener("click", randomChangeColor);
